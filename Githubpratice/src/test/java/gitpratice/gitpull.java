d9a7e8dc630ea64aef61c6f5f5533bac9d7c1e52
hp <hp@DESKTOP-F33EGAS>
2021-11-11 17:23:22 +0530

    SAME STATEMENT

--- Githubpratice/src/test/java/gitpratice/gitpull.java
+++ Githubpratice/src/test/java/gitpratice/gitpull.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class gitpull {@Test
 	public void gitpush() {
 	System.out.println("User 2 First Push");
+	System.out.println("User 2 First Push");
 	System.out.println("User 2 2nd Push");
 	System.out.println("User 2 3rd Push");
 }
