56d198516fb3b91f0b23f666f57a7fa12ffa7a8f
hp <hp@DESKTOP-F33EGAS>
2021-11-12 12:04:16 +0530

    push

--- Githubpratice/src/test/java/gitpratice/gitpull.java
+++ Githubpratice/src/test/java/gitpratice/gitpull.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class gitpull {@Test
 	public void gitpush() {
 	System.out.println("User 2 First Push");
-	System.out.println("12-11-2021");
+	System.out.println("12-11-2021 new");
 	System.out.println("User 2 First Push");
 	System.out.println("User 2 2nd Push");
 	System.out.println("User 2 3rd Push");
