9adec5f1ddb9f8849f76a9a39853d4f2d2b1a47c
hp <hp@DESKTOP-F33EGAS>
2021-11-12 18:04:11 +0530

    s

---
+++ Githubpratice/src/test/java/gitpratice/RegressionTest.java
@@ -0,0 +1,11 @@
+package gitpratice;
+
+import org.testng.annotations.Test;
+
+public class RegressionTest {
+	@Test
+	public void RegressionTest1() {
+		System.out.println("REGRESSION TEST");
+	}
+
+}
